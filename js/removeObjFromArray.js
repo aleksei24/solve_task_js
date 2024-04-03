e23313cb774da0d54862af1be905dd4025d7fc5e
Aleksei <lexa24032@gmail.com>
2024-04-03 18:39:13 +0300

    now an object is removed from an array

--- js/removeObjFromArray.js
+++ js/removeObjFromArray.js
@@ -1,9 +1,8 @@
-// was
-var arraydata = [
+let arraydata = [
   { name: 'name1', value: 1 },
   { name: 'name2', value: 2 },
   { name: 'name3', value: 3 },
 ];
 
-arraydata.filter((i) => i.value !== 1);
+arraydata = arraydata.filter((i) => i.value !== 3);
 console.log(arraydata);
