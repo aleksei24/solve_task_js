76a2a1642bc41680383e6c4abf8ea3133cb1fa7a
Aleksei <lexa24032@gmail.com>
2024-04-03 18:35:41 +0300

    trying to remove objects from an array, unsuccsessfully

---
+++ js/removeObjFromArray.js
@@ -0,0 +1,9 @@
+// was
+var arraydata = [
+  { name: 'name1', value: 1 },
+  { name: 'name2', value: 2 },
+  { name: 'name3', value: 3 },
+];
+
+arraydata.filter((i) => i.value !== 1);
+console.log(arraydata);
